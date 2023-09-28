529ea343d205aa0ad2503e8140e4ff82dc3b4135
TEC <git@tecosaur.net>
2023-09-28 21:56:04 +0800

    Rewrite keccak to use a state tuple
    Special thanks to Lilith Halfner for working this out.

--- src/KangarooTwelve.jl
+++ src/KangarooTwelve.jl
@@ -12,123 +12,137 @@ module KangarooTwelve
 import Base.Cartesian.@ntuple
 
 const ROUND_CONSTS_24 =
-    [0x0000000000000001, 0x0000000000008082, 0x800000000000808a, 0x8000000080008000,
+    (0x0000000000000001, 0x0000000000008082, 0x800000000000808a, 0x8000000080008000,
      0x000000000000808b, 0x0000000080000001, 0x8000000080008081, 0x8000000000008009,
      0x000000000000008a, 0x0000000000000088, 0x0000000080008009, 0x000000008000000a,
      0x000000008000808b, 0x800000000000008b, 0x8000000000008089, 0x8000000000008003,
      0x8000000000008002, 0x8000000000000080, 0x000000000000800a, 0x800000008000000a,
-     0x8000000080008081, 0x8000000000008080, 0x0000000080000001, 0x8000000080008008]
+     0x8000000080008081, 0x8000000000008080, 0x0000000080000001, 0x8000000080008008)
 
-const ρs = UInt64[
-    1,  3,  6,  10, 15, 21, 28, 36, 45, 55,  2, 14,
-    27, 41, 56,  8, 25, 43, 62, 18, 39, 61, 20, 44]
+const ρs = UInt64.(
+    (0, 44, 43, 21, 14, 28, 20, 3, 45, 61, 1, 6, 25,
+     8, 18, 27, 36, 10, 15, 56, 62, 55, 39, 41, 2))
 
-const πs = Int[
-    11,  8, 12, 18, 19, 4,  6,  17, 9, 22, 25, 5,
-    16, 24, 20, 14, 13, 3, 21, 15, 23, 10,  7, 2]
+const πs =
+    (1, 7, 13, 19, 25, 4, 10, 11, 17, 23, 2, 8, 14,
+     20, 21, 5, 6, 12, 18, 24, 3, 9, 15, 16, 22)
 
-# The `keccak_p1600!` function currently takes ~290ns / 1500 cycles
-# to execute. The K12 paper states about their C implementation:
-#   "On an Intel Core i5-6500 (Skylake), we measured that one evaluation of
-#    Keccak-p[1600, nr = 12] takes about 450 cycles"
-# This suggests that we /should/ be able to get this down to ~90ns per
-# evaluation.
+const EMPTY_STATE = Tuple(zeros(UInt64, 25))
 
-function keccak_p1600!(state::Vector{UInt64}, ::Val{nrounds}=Val{12}()) where {nrounds}
-    @boundscheck length(state) == 25 || throw(ArgumentError("`state` must be of length exactly 25 (not $(length(state)))"))
-    rol64(a, n) = (a >> (64 - (n % 64))) | (a << (n % 64))
-    @inbounds for rc in view(ROUND_CONSTS_24, (25-nrounds):24)
+function keccak_p1600(state::NTuple{25, UInt64}, ::Val{nrounds}=Val{12}()) where {nrounds}
+    rol64(a, n) = (a << n) | (a >> (64 - n))
+    # ~90ns
+    @inbounds for round in (25 - nrounds):24
         # θ (diffusion)
-        C = @ntuple 5 i -> reduce(⊻, @ntuple 5 k -> state[i+5*(k-1)]) # ~1ns
-        D = @ntuple 5 i -> C[mod1(i+4, 5)] ⊻ rol64(C[mod1(i+1, 5)], 1) # ~0ns
-        for i in 1:25 # ~120ns
-            state[i] ⊻= D[mod1.(i, 5)]
-        end
-        # # ρ (rotation) and π (lane permutation)
-        last = state[πs[end]]
-        for i in 1:24 # ~100ns
-            j = state[πs[i]]
-            state[πs[i]] = rol64(last, ρs[i])
-            last = j
-        end
-        # # χ (intra-row bitwise combination, nonlinear)
-        for j in 0:5:20 # ~50ns
-            C = @ntuple 5 i -> state[j + i]
-            for i in 1:5
-                state[j + i] = C[i] ⊻ (~C[mod1(i+1, 5)] & C[mod1(i+2, 5)])
-            end
-        end
+        C = @ntuple 5 i -> reduce(⊻, @ntuple 5 k -> state[i+5*(k-1)]) # ~12ns
+        D = @ntuple 5 i -> C[mod1(i+4, 5)] ⊻ rol64(C[mod1(i+1, 5)], 1) # ~20ns
+        state = @ntuple 25 i -> state[i] ⊻ D[mod1(i, 5)] # ~7ns
+        # ρ (rotation) and π (lane permutation)
+        state = @ntuple 25 i -> rol64(state[πs[i]], ρs[i]) # ~6ns
+        # χ (intra-row bitwise combination, nonlinear)
+        state = ( # ~10ns
+            state[ 1] ⊻ (~state[ 2] & state[ 3]),
+            state[ 2] ⊻ (~state[ 3] & state[ 4]),
+            state[ 3] ⊻ (~state[ 4] & state[ 5]),
+            state[ 4] ⊻ (~state[ 5] & state[ 1]),
+            state[ 5] ⊻ (~state[ 1] & state[ 2]),
+            state[ 6] ⊻ (~state[ 7] & state[ 8]),
+            state[ 7] ⊻ (~state[ 8] & state[ 9]),
+            state[ 8] ⊻ (~state[ 9] & state[10]),
+            state[ 9] ⊻ (~state[10] & state[ 6]),
+            state[10] ⊻ (~state[ 6] & state[ 7]),
+            state[11] ⊻ (~state[12] & state[13]),
+            state[12] ⊻ (~state[13] & state[14]),
+            state[13] ⊻ (~state[14] & state[15]),
+            state[14] ⊻ (~state[15] & state[11]),
+            state[15] ⊻ (~state[11] & state[12]),
+            state[16] ⊻ (~state[17] & state[18]),
+            state[17] ⊻ (~state[18] & state[19]),
+            state[18] ⊻ (~state[19] & state[20]),
+            state[19] ⊻ (~state[20] & state[16]),
+            state[20] ⊻ (~state[16] & state[17]),
+            state[21] ⊻ (~state[22] & state[23]),
+            state[22] ⊻ (~state[23] & state[24]),
+            state[23] ⊻ (~state[24] & state[25]),
+            state[24] ⊻ (~state[25] & state[21]),
+            state[25] ⊻ (~state[21] & state[22]),
+        )
         # ι (symmetry disruptor)
-        state[1] ⊻= rc # ~1ns
+        state = Base.setindex(state, state[1] ⊻ ROUND_CONSTS_24[round], 1) # ~3ns
     end
     state
 end
 
-function ingest!(state::Vector{UInt64}, ::Val{capacity}, message::Vector{UInt64}) where {capacity}
+function ingest!(state::NTuple{25, UInt64}, ::Val{capacity}, message::AbstractVector{UInt64}) where {capacity}
     rate = 25 - capacity ÷ 64
     for block in Iterators.partition(message, rate)
-        view(state, 1:length(block)) .⊻= block
-        keccak_p1600!(state)
+        state = if length(block) == rate
+            @ntuple 25 i -> if i <= rate
+                state[i] ⊻ block[i]
+            else state[i] end
+        else
+            @ntuple 25 i -> state[i] ⊻ get(block, i, zero(UInt64))
+        end |> keccak_p1600
     end
-    mod1(length(message), rate)
+    state, mod1(length(message), rate)
 end
 
-function pad!(state::Vector{UInt64}, ::Val{capacity}, finalblk::Int, delimsufix::UInt64) where {capacity}
+function pad!(state::NTuple{25, UInt64}, ::Val{capacity}, finalblk::Int, delimsufix::UInt64) where {capacity}
     rate = 25 - capacity ÷ 64
-    state[finalblk] ⊻= delimsufix
-    state[rate-1] ⊻= UInt64(0x80)
-    keccak_p1600!(state)
+    state = Base.setindex(state, state[finalblk] ⊻ delimsufix, finalblk)
+    state = Base.setindex(state, state[rate-1] ⊻ UInt64(0x80), rate-1)
+    keccak_p1600(state)
 end
 
-function squeeze!(state::Vector{UInt64}, ::Val{capacity}, ::Val{output}) where {capacity, output}
+function squeeze!(state::NTuple{25, UInt64}, ::Val{capacity}, ::Val{output}) where {capacity, output}
     rate = 25 - capacity ÷ 64
     if output ÷ 64 <= rate
-        view(state, 1:output÷64)
+        state[1:output÷64]
     else
         squeeze!(zeros(UInt64, output÷64), state, Val{capacity}())
     end
 end
 
-function squeeze!(output::Vector{UInt64}, state::Vector{UInt64}, ::Val{capacity}) where {capacity}
+function squeeze!(output::Vector{UInt64}, state::NTuple{25, UInt64}, ::Val{capacity}) where {capacity}
     rate = 25 - capacity ÷ 64
     if length(output) <= rate
-        copyto!(output, 1, state, 1, output)
+        output .= state[1:length(output)]
     else
         index = 1
         while index < length(output)
-            index == 1 || keccak_p1600!(state)
+            index == 1 || (state = keccak_p1600(state))
             bsize = min(rate, length(output) - index + 1)
-            copyto!(output, index, state, 1, bsize)
+            output[index:index+bsize] .= state[1:bsize]
             index += bsize
         end
     end
     output
 end
 
-function turboshake!(state::Vector{UInt64}, capacity::Val, message::Vector{UInt64},
-                     delimsufix::UInt64=UInt64(0x80), output::Val=(function (::Val{c}) where {c} Val{c÷2}() end)(capacity))
-    finalblk = ingest!(state, capacity, message)
-    pad!(state, capacity, finalblk, delimsufix)
+function turboshake(state::NTuple{25, UInt64}, capacity::Val, message::AbstractVector{UInt64},
+                    delimsufix::UInt64=UInt64(0x80), output::Val=(function (::Val{c}) where {c} Val{c÷2}() end)(capacity))
+    state, finalblk = ingest!(state, capacity, message)
+    state = pad!(state, capacity, finalblk, delimsufix)
     squeeze!(state, capacity, output)
 end
 
-turboshake(args...) = turboshake!(zeros(UInt64, 25), args...)
+turboshake(args...) = turboshake(EMPTY_STATE, args...)
 
 ## Testing
 
-function throughput(::typeof(keccak_p1600!), size) # 2 = 1KiB, 2 = 32KiB, 4 = 1MiB...
-    state = zeros(UInt64, 25)
+function throughput(::typeof(keccak_p1600), size) # 2 = 1KiB, 2 = 32KiB, 4 = 1MiB...
+    state = Tuple(zeros(UInt64, 25))
     rounds = round(Int, 32^size / 200)
     start = time()
     for _ in 1:rounds
-        keccak_p1600!(state)
+        state = keccak_p1600(state)
     end
     stop = time()
     push!([], state) # prevent the call from being optimised away
     println(" Keccak-p[1600,12] throughput: ~$(round(Int, 32^size / (stop - start) / 1024^2)) MiB/s")
 end
 
-function throughput(::typeof(turboshake), size) # 2 = 1KiB, 2 = 32KiB, 4 = 1MiB...
+function throughput(::typeof(turboshake), size)
     message = rand(UInt64, round(Int, 32^size) ÷ 8)
     start = time()
     x = turboshake(Val(256), message)
@@ -139,7 +153,7 @@ end
 
 function throughput(step::Symbol, size)
     func = if step == :keccak
-        keccak_p1600!
+        keccak_p1600
     elseif step == :turboshake
         turboshake
     end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,47 +1,44 @@
 using KangarooTwelve
 using Test
 
-@testset "turboshake128" begin
+@testset "keccak" begin
     keccak_1600_init =
-        [0xf1258f7940e1dde7, 0x84d5ccf933c0478a, 0xd598261ea65aa9ee, 0xbd1547306f80494d,
-        0x8b284e056253d057, 0xff97a42d7f8e6fd4, 0x90fee5a0a44647c4, 0x8c5bda0cd6192e76,
-        0xad30a6f71b19059c, 0x30935ab7d08ffc64, 0xeb5aa93f2317d635, 0xa9a6e6260d712103,
-        0x81a57c16dbcf555f, 0x43b831cd0347c826, 0x01f22f1a11a5569f, 0x05e5635a21d9ae61,
-        0x64befef28cc970f2, 0x613670957bc46611, 0xb87c5a554fd00ecb, 0x8c3ee88a1ccf32c8,
-        0x940c7922ae3a2614, 0x1841f924a2c509e4, 0x16f53526e70465c2, 0x75f644e97f30a13b,
-        0xeaf1ff7b5ceca249]
+        (0xf1258f7940e1dde7, 0x84d5ccf933c0478a, 0xd598261ea65aa9ee, 0xbd1547306f80494d,
+         0x8b284e056253d057, 0xff97a42d7f8e6fd4, 0x90fee5a0a44647c4, 0x8c5bda0cd6192e76,
+         0xad30a6f71b19059c, 0x30935ab7d08ffc64, 0xeb5aa93f2317d635, 0xa9a6e6260d712103,
+         0x81a57c16dbcf555f, 0x43b831cd0347c826, 0x01f22f1a11a5569f, 0x05e5635a21d9ae61,
+         0x64befef28cc970f2, 0x613670957bc46611, 0xb87c5a554fd00ecb, 0x8c3ee88a1ccf32c8,
+         0x940c7922ae3a2614, 0x1841f924a2c509e4, 0x16f53526e70465c2, 0x75f644e97f30a13b,
+         0xeaf1ff7b5ceca249)
 
     keccak_1600_init12 =
-        [0x32bdb29984dd0136, 0xd007ccf81390f00d, 0x255f7d637de64fab, 0x1e68c397bd8851b5,
-        0x42650061d07bbd1d, 0x8904bc882d358cd1, 0x59861999977d5625, 0x3f58254b6d09b4b3,
-        0xd16ca2220abb1d9c, 0x15f1da0b4c67b71b, 0xd65da28284ee7fb3, 0x286b5606107b116b,
-        0xec032b5abf13af97, 0xb917b75d3d045350, 0x673d897d6f671820, 0x28d0476c209df0f0,
-        0x0c6182b878ce4246, 0xb83f00cd9db7feff, 0x1a1419f9a37814c8, 0x29e53860a4af645f,
-        0x6922db03f53e69e8, 0x36c30f12e8bb4606, 0x1a30b0200c06b2e2, 0xea5dc7c65491c36a,
-        0xc1de6860d0d10358]
+        (0x32bdb29984dd0136, 0xd007ccf81390f00d, 0x255f7d637de64fab, 0x1e68c397bd8851b5,
+         0x42650061d07bbd1d, 0x8904bc882d358cd1, 0x59861999977d5625, 0x3f58254b6d09b4b3,
+         0xd16ca2220abb1d9c, 0x15f1da0b4c67b71b, 0xd65da28284ee7fb3, 0x286b5606107b116b,
+         0xec032b5abf13af97, 0xb917b75d3d045350, 0x673d897d6f671820, 0x28d0476c209df0f0,
+         0x0c6182b878ce4246, 0xb83f00cd9db7feff, 0x1a1419f9a37814c8, 0x29e53860a4af645f,
+         0x6922db03f53e69e8, 0x36c30f12e8bb4606, 0x1a30b0200c06b2e2, 0xea5dc7c65491c36a,
+         0xc1de6860d0d10358)
 
     keccak_1600_12rounds =
-        [0x8b90f350b5018696, 0xbbd981435d1b7440, 0xc3907f0b1cf9fa9b, 0x650b8934855a881f,
-        0x3acb12c9505d6125, 0x58aa8e6240715d20, 0x6dace6cf13225fcb, 0x17f2a9b889bad731,
-        0x705552894792eb44, 0x39f3c319d2bfb4a0, 0x6ccbc0b5a1a26f58, 0xa59599376203c84f,
-        0xb0a7f75e71b11faa, 0xf41131da8a5aceb4, 0xb780be5266849655, 0x67e882ed196882ca,
-        0x669327075f0c94bb, 0x8a2c7727a5a3b612, 0x5e0d91393c42af39, 0xfa849e4b42de2f42,
-        0x9c54fe0e1fd87d9b, 0x2bc0f29fd366b762, 0xe12aa82fc75e1704, 0x2d4bac302a4c445a,
-        0x365f02c25ea643ee]
+        (0x8b90f350b5018696, 0xbbd981435d1b7440, 0xc3907f0b1cf9fa9b, 0x650b8934855a881f,
+         0x3acb12c9505d6125, 0x58aa8e6240715d20, 0x6dace6cf13225fcb, 0x17f2a9b889bad731,
+         0x705552894792eb44, 0x39f3c319d2bfb4a0, 0x6ccbc0b5a1a26f58, 0xa59599376203c84f,
+         0xb0a7f75e71b11faa, 0xf41131da8a5aceb4, 0xb780be5266849655, 0x67e882ed196882ca,
+         0x669327075f0c94bb, 0x8a2c7727a5a3b612, 0x5e0d91393c42af39, 0xfa849e4b42de2f42,
+         0x9c54fe0e1fd87d9b, 0x2bc0f29fd366b762, 0xe12aa82fc75e1704, 0x2d4bac302a4c445a,
+         0x365f02c25ea643ee)
 
     keccak_1600_24rounds =
-        [0x2d5c954df96ecb3c, 0x6a332cd07057b56d, 0x093d8d1270d76b6c, 0x8a20d9b25569d094,
-        0x4f9c4f99e5e7f156, 0xf957b9a2da65fb38, 0x85773dae1275af0d, 0xfaf4f247c3d810f7,
-        0x1f1b9ee6f79a8759, 0xe4fecc0fee98b425, 0x68ce61b6b9ce68a1, 0xdeea66c4ba8f974f,
-        0x33c43d836eafb1f5, 0xe00654042719dbd9, 0x7cf8a9f009831265, 0xfd5449a6bf174743,
-        0x97ddad33d8994b40, 0x48ead5fc5d0be774, 0xe3b8c8ee55b7b03c, 0x91a0226e649e42e9,
-        0x900e3129e7badd7b, 0x202a9ec5faa3cce8, 0x5b3402464e1c3db6, 0x609f4e62a44c1059,
-        0x20d06cd26a8fbf5c]
+        (0x2d5c954df96ecb3c, 0x6a332cd07057b56d, 0x093d8d1270d76b6c, 0x8a20d9b25569d094,
+         0x4f9c4f99e5e7f156, 0xf957b9a2da65fb38, 0x85773dae1275af0d, 0xfaf4f247c3d810f7,
+         0x1f1b9ee6f79a8759, 0xe4fecc0fee98b425, 0x68ce61b6b9ce68a1, 0xdeea66c4ba8f974f,
+         0x33c43d836eafb1f5, 0xe00654042719dbd9, 0x7cf8a9f009831265, 0xfd5449a6bf174743,
+         0x97ddad33d8994b40, 0x48ead5fc5d0be774, 0xe3b8c8ee55b7b03c, 0x91a0226e649e42e9,
+         0x900e3129e7badd7b, 0x202a9ec5faa3cce8, 0x5b3402464e1c3db6, 0x609f4e62a44c1059,
+         0x20d06cd26a8fbf5c)
 
-    @test KangarooTwelve.keccakP1600(copy(keccak_1600_init)) ==
-        keccak_1600_12rounds
-    @test KangarooTwelve.keccakP1600(copy(keccak_1600_init12)) ==
-        keccak_1600_24rounds
-    @test KangarooTwelve.keccakP1600(copy(keccak_1600_init), Val(24)) ==
-        keccak_1600_24rounds
+    @test KangarooTwelve.keccak_p1600(keccak_1600_init) == keccak_1600_12rounds
+    @test KangarooTwelve.keccak_p1600(keccak_1600_init12) == keccak_1600_24rounds
+    @test KangarooTwelve.keccak_p1600(keccak_1600_init, Val(24)) == keccak_1600_24rounds
 end
