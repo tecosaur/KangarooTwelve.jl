561c6e4a2b810e98261e702948c77954914d69f4
TEC <git@tecosaur.net>
2023-11-02 00:35:18 +0800

    Add sponge squeezing tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -71,6 +71,22 @@ end
     @test ingest(sponge, 0x22222222, 0x1111111111111111).state[1] == 0x0000000011111111
 end
 
+@testset "Sponge squeezing" begin
+    sponge = Sponge{21}(reinterpret(NTuple{25, UInt64}, Tuple((UInt8(i) for i in 1:200))), 1)
+    @test squeeze(UInt8, sponge) == 0x01
+    @test squeeze(UInt16, sponge) == 0x0201
+    @test squeeze(UInt32, sponge) == 0x04030201
+    @test squeeze(UInt64, sponge) == 0x0807060504030201
+    @test squeeze(UInt128, sponge) == 0x100f0e0d0c0b0a090807060504030201
+    # Representational equivalence
+    @test reinterpret(UInt128, squeeze(NTuple{2, UInt64}, sponge)) == 0x100f0e0d0c0b0a090807060504030201
+    @test reinterpret(UInt128, squeeze(NTuple{4, UInt32}, sponge)) == 0x100f0e0d0c0b0a090807060504030201
+    @test reinterpret(UInt128, squeeze(NTuple{8, UInt16}, sponge)) == 0x100f0e0d0c0b0a090807060504030201
+    @test reinterpret(UInt128, squeeze(NTuple{16, UInt8}, sponge)) == 0x100f0e0d0c0b0a090807060504030201
+    # Wrap around
+    @test squeeze(NTuple{22, UInt64}, sponge)[end] == first(keccak_p1600(sponge.state))
+end
+
 @testset "Length encoding" begin
     @test ingest_length(Sponge(), 0).state[1] == 0x0000000000000000
     @test ingest_length(Sponge(), 12).state[1] == 0x000000000000010c
